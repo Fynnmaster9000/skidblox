58ff2baee541a574d90fd0fe041391ccc1d649fa
Fynnmaster9000 <fynnmaster9000@gmail.com>
2025-06-01 18:36:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("downloadBtn").addEventListener("click", function() {
+    window.location.href = "https://github.com/Fynnmaster9000/skidblox/releases/download/skidbloxv2/skidblox.zip"; // Replace with your actual file URL
+});
